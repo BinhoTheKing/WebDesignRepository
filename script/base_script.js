112e73a8c10faff99bd1fbe42bc95d2f97f11813
Fabio <fabioamorim1501@gmail.com>
2015-11-19 14:32:37 -0200

    Some tabs added (not aligned to right ¬¬)

--- script/base_script.js
+++ script/base_script.js
@@ -1,5 +1,32 @@
-angular.module('toolbarDemo1', ['ngMaterial'])
-        .controller('AppCtrl', function ($scope) {
-        });
+var app = angular.module('toolbarDemo1', ['ngMaterial']);
+app.config(function ($mdThemingProvider) {
+//    // Extend the red theme with a few different colors
+//    var darkGreyMap = $mdThemingProvider.extendPalette('grey', {
+//        '100': '#595959',
+//        '500': '#3D3D3D',
+//        '700': '#474747',
+//        'contrastDefaultColor': 'light',
+//        'contrastDarkColors': ['100']
+//        
+//    });
+//    
+//    var reallyRedMap = $mdThemingProvider.extendPalette('red',{
+//       '400' : '#E84C3D'
+//    });
+//    // Register the new color palette map with the name <code>darkGrey</code>
+//    $mdThemingProvider.definePalette('darkGrey', darkGreyMap);
+//    $mdThemingProvider.definePalette('reallyRed', reallyRedMap);
+//    // Use that theme for the primary intentions
+//    $mdThemingProvider.theme('default')
+//            .primaryPalette('darkGrey')
+//            .accentPalette('reallyRed');
+    $mdThemingProvider.theme('default')
+            .primaryPalette('grey')
+            .accentPalette('red');
+});
+app.controller('AppCtrl', function ($scope) {
+});
+
+
 
 
